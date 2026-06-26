e61ae35cafb0e2905f36dd04f5520ac71b277107
sarvins <33715976+sarvins@users.noreply.github.com>
2026-06-26 17:47:51 +0200

    Add weather checking and SMS notification features
    Refactor code to integrate weather API and SMS notifications.

--- main.py
+++ main.py
@@ -1,38 +1,59 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
+import requests
 import os
+from twilio.rest import Client
+from dotenv import load_dotenv
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+weather_url:str = "https://api.openweathermap.org/data/2.5/forecast"
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+load_dotenv()
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+weather_api_key = os.environ.get("WEATHER_API_KEY")
+
+params = {
+  'lat':37.477421,
+  'lon':-86.294144, 
+  'appid':weather_api_key,
+  'cnt':4
+}
+
+response = requests.get(url=weather_url, params=params)
+response.raise_for_status()
+weather_data = response.json()
+print(weather_data)
+
+
+# create id's for the day
+def check_weather() -> bool:
+  will_rain = False
+  for hour_data in weather_data['list']:
+    condition_code = hour_data['weather'][0]['id']
+    if int(condition_code) < 700:
+      will_rain = True
+  return will_rain
+
+def send_sms():
+  will_rain = check_weather()
+  account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
+  auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
+  number_send = os.environ.get("MOBILE_NUMBER_SEND")
+  number_receive = os.environ.get["MOBILE_NUMBER_RECEIVE"]
+  client = Client(account_sid, auth_token)
+  
+  if will_rain:
+    message = client.messages.create(
+      body="It will rain today! take an umbrella!",
+      from_=number_send,
+      to=number_receive,
+    )
+  else:
+      message = client.messages.create(
+      body="It will not rain today!",
+      from_= number_send,
+      to=number_receive,
+    )
+      
+  print(f"message was sent: {message.status}")
+
+print(check_weather())
+send_sms()
